1df7c929f4dfe9da66c7d4bebf9e6a238baa8bd1
Koji Sakamoto <kowwwwji@gmail.com>
2021-02-03 03:52:37 +0900

    add/googleSearchまで

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Google from './Google';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Google />
   </React.StrictMode>,
   document.getElementById('root')
 );
